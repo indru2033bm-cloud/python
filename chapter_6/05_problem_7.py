5b4489baed723faa159d80e1b745bc5bf0cf2e96
Indrajith <indru2033bm@gmail.com>
2026-02-09 12:20:52 +0530

    corrected some mistake in problem

--- chapter_6/05_problem_7.py
+++ chapter_6/05_problem_7.py
@@ -1,8 +1,6 @@
-a = input("Enter your resived post or not (answer in yes or no ): ")
+a = input("Enter your post : ")
 
-if a == "yes":
-    print("Your quarier is resived")
-elif a == "no":
-    print("Your quarier is not resived")
+if "Indrajith" in a.lower():
+    print("Yes, this post is about Indrajith")
 else:
-    print("You have entered an invalid answer")        
+    print("No, this post is not about Indrajith")    
