487e139d440352d51bb6ab10d1514f57f5baee50
Indrajith <indru2033bm@gmail.com>
2026-02-09 11:08:07 +0530

    chapter_5 additional problemes

---
+++ chapter_5/06_problem_6.py
@@ -0,0 +1,12 @@
+lang = {}
+l = input("Enetr the languge : ") 
+n = input("Enter the name: ")
+lang.update({l: n})
+l = input("Enetr the languge : ") 
+n = input("Enter the name: ")
+lang.update({l: n})
+l = input("Enetr the languge : ") 
+n = input("Enter the name: ")
+lang.update({l: n})
+
+print(lang)
