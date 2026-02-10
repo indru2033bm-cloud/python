5a4c311d11f094195812c0455b399549d394b9c8
Indrajith <indru2033bm@gmail.com>
2026-02-10 19:47:49 +0530

    modified

--- chapter_5/02_dis_method.py
+++ chapter_5/02_dis_method.py
@@ -7,7 +7,7 @@ marks = {
 
 # print(marks.items()) #returns a list of tuples
 # print(marks.keys()) #returns a list of keys
-# print(marks.values()) #returns a list of values
+print(marks.values()) #returns a list of values
 # marks.update({"Indrajith":99, "Ramashekar":98 }) #update the value of a key and add a new key-value pair
 # print(marks)
 print(marks.get("Indrajith2")) #returns the value of the key but does not raise an error if the key is not found, instead returns None
