57d9f32a068c93da9d5e0efc93d19210a1e8580d
Indrajith <indru2033bm@gmail.com>
2026-02-15 22:56:49 +0530

    Main page of the magaproduct

--- maga_project/main.py
+++ maga_project/main.py
@@ -8,5 +8,35 @@ def speck(text):
     engine.say(text)
     engine.runAndWait()
 if __name__ == "__main__":
-    speck("Hello, I am your assistant. How can I help you?")
+    speck("Initializing Jarivis......")
+    while True:
+        #listen for the word "Jarivis"
+        #obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio = r.listen(source)
     
+# recc ognize speech using sphinx
+        try:
+            command = r.recognize_sphinx(audio)
+            print(command)
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+        if "Jarivis" in command:
+            speck("Yes, how can I help you?")
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source)
+            try:
+                command = r.recognize_sphinx(audio)
+                print(command)
+            except sr.UnknownValueError:
+                print("Sphinx could not understand audio")
+            except sr.RequestError as e:
+                print("Sphinx error; {0}".format(e))
+            if "open YouTube" in command:
+                webbrowser.open("https://www.youtube.com")
+                speck("Opening YouTube")
